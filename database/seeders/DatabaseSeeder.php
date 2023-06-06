e9ac25bc53ea342cfbef89ba539adb3a586b511a
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-06 13:10:16 +0200

    feat: bonus - seeder for Technology model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Technology;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,7 +20,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TypeSeeder::class,
-            ProjectSeeder::class
+            ProjectSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['HTML', 'CSS', 'Scss', 'JS', 'VueJS', 'React', 'Angular', 'PHP', 'Laravel'];
+
+        //Schema::disableForeignKeyConstraints(); // disabilita la chiave esterna
+        Technology::truncate(); // svuota la tabella
+        //Schema::enableForeignKeyConstraints(); // riabilita la chiave esterna
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name, '-');
+
+            $new_technology->save();
+        }
+    }
+}
